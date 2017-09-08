0ca76cfd1eee222d72ca70027508562cc21d866f
John D. Han <unimunity@gmail.com>
2017-09-09 00:28:37 +0900

    Add qna app

--- AtSpot/settings.py
+++ AtSpot/settings.py
@@ -49,7 +49,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # django app
-    'inquiries',
 )
 
 MIDDLEWARE_CLASSES = (
--- qna/models.py
+++ qna/models.py
@@ -1,4 +1,9 @@
 from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
