ae9158a5cae01e154f8bc52b809512a7580e893c
RyuDongIl <jules010@naver.com>
2017-09-09 06:57:50 +0900

    일단 되넉ㄱ같음 자고싶음

--- qna/serializers.py
+++ qna/serializers.py
@@ -1,9 +1,9 @@
-from rest_framework import serializers  
+from rest_framework import serializers
 from qna.models import Inquiry
 from qna.models import Answer
 
 
-class InquirySerializer(serializers.ModelSerializer):  
+class InquirySerializer(serializers.ModelSerializer):
     class Meta:
         model = Inquiry
         fields = ('id', 'contents', 'point', 'created_on', 'owner')
@@ -11,4 +11,4 @@ class InquirySerializer(serializers.ModelSerializer):
 class AnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Answer
-        fields = ('id', 'owner', 'inquiry', 'image')
+        fields = ('id', 'owner', 'inquiry', 'contents', 'image')
--- qna/views.py
+++ qna/views.py
@@ -6,26 +6,39 @@ from accounts.models import Profile
 
 from qna.serializers import InquirySerializer
 from qna.serializers import AnswerSerializer
-from rest_framework import generics
-from rest_framework import viewsets
+from rest_framework import generics, viewsets, renderers
+from rest_framework.response import Response
 from rest_framework.decorators import detail_route
 
 class InquiryViewSet(viewsets.ModelViewSet):
     queryset = Inquiry.objects.all()
     serializer_class = InquirySerializer
 
+
     def perform_create(self, serializer):
-            serializer.save(owner=Profile.objetcs.get(user=self.request.user))
+        serializer.save(owner=Profile.objects.get(user=self.request.user))
+
+    @detail_route(renderer_classes=[renderers.JSONRenderer])
+    def get_answer(self, request, *args, **kwargs):
+        answer = Answer.objects.filter(inquiry=self.get_object()).last()
+        serializer = AnswerSerializer(answer)
+        return Response(serializer.data)
+
+
+    # @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
+    # def answers_to_inquiry(self, request, *args, **kwargs):
+    #     snippet = self.objects.filter()
+    #     return Response(snippet.highlighted)
 
 class AnswerViewSet(viewsets.ModelViewSet):
     queryset = Answer.objects.all()
     serializer_class = AnswerSerializer
 
-
-# @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
-#     def answers_to_inquiry(self, request, *args, **kwargs):
-#         snippet = self.objects.filter()
-#         return Response(snippet.highlighted)
-
     def perform_create(self, serializer):
-            serializer.save(owner=Profile.objetcs.get(user=self.request.user))
+            serializer.save(owner=Profile.objects.get(user=self.request.user))
+
+    @detail_route(renderer_classes=[renderers.JSONRenderer])
+    def answer_to_inquiry(self, request, pk):
+        answer = Answer.objects.filter(inquiry=pk).last()
+        serializer = AnswerSerializer(answer)
+        return Response(serializer.data)
