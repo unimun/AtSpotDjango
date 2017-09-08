793d3d36830f61da33f8b90da9b543345218de12
RyuDongIl <jules010@naver.com>
2017-09-08 20:59:56 +0900

    settings installed apps에 inquiries추가

--- AtSpot/settings.py
+++ AtSpot/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # django app
+    'inquiries',
 )
 
 MIDDLEWARE_CLASSES = (
