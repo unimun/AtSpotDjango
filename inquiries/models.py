5c2161a39ac6adbecf7f130c032bf9de14a84cc2
John D. Han <unimunity@gmail.com>
2017-09-08 20:48:34 +0900

    Add inquiry model & set up build config

--- inquiries/models.py
+++ inquiries/models.py
@@ -1,3 +1,23 @@
+from __future__ import unicode_literals
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Inquiry(models.Model):
+
+    owner = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+        related_name='inquiry',
+    )
+    subject = models.CharField(max_length=30, null=True)
+    body = models.TextField(max_length=30, null=True)
+
+    created_on = models.DateTimeField(auto_now_add=True)
+    expires_in = models.IntegerField(null=True)
+
+    class Meta:
+        ordering = ('created_on', )
+
+    def __unicode__(self):
+        return self.owner.username + ' ' + str(self.subject)
